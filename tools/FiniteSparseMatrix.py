87dc13d7a9bdb36a4ea5bb875ca20783b44005e3
George Coote <108678340+GeorgeCoote@users.noreply.github.com>
2025-12-26 12:14:55 +0000

    Update FiniteSparseMatrix.py

--- tools/FiniteSparseMatrix.py
+++ tools/FiniteSparseMatrix.py
@@ -12,21 +12,57 @@ class FiniteSparseMatrix:
         B_size = len(B.entries)
         smaller, bigger = self, B if self_size <= B_size else B, self
         new_entries = bigger.entries.copy()
+        
         for idx in smaller.entries:
             candidate = new_entries.get(idx, 0.0) + smaller.entries[idx]
             if abs(candidate - new_default) < 1e-16:
                 new_entries.pop(idx, None)
+                
             else:
-                new_entries[idx] = candidate                
+                new_entries[idx] = candidate 
+                
         return FiniteSparseMatrix(new_entries, new_default)
 
     def __mul__(self, c : float) -> 'FiniteSparseMatrix':
         if c < 1e-16:
             return FiniteSparseMatrix({}, 0.0)
+            
         else:
             new_entries = {key : c*val for key, val in self.entries.items()}
             new_default = c*self.default
             return FiniteSparseMatrix(new_entries, new_default)
+
+    def __matmul__(self, B : 'FiniteSparseMatrix') -> 'FiniteSparseMatrix':
+        if B.default != 0.0 or self.default != 0.0:
+            raise ValueError("Default value of both self and B should be 0.0")
+            
+        if not B.entries or not self.entries:
+            return FiniteSparseMatrix({}, 0.0)
+            
+        i_vals_self = set([elt[0] for elt, _ in self.entries.items()])
+        
+        i_vals_B = set([elt[0] for elt, _ in B.entries.items()])
+        max_i_vals_B = max(i_vals_B)
+        
+        j_vals_self = set([elt[1] for elt, _ in self.entries.items()])
+        max_j_vals_self = max(j_vals_self)
+        
+        j_vals_B = set([elt[1] for elt, _ in B.entries.items()])
+        
+        upper_bound = min(max_i_vals_B, max_j_vals_self) + 1
+
+        new_entries = {}
+        
+        for i in i_vals_self:
+            for j in j_vals_B:
+                candidate = 0.0
+                for k in range(0, upper_bound):
+                    candidate += self(i, k)*B(k, j)
+                    
+                if abs(candidate) > 1e-16:
+                    new_entries[(i, j)] = candidate
+
+        return FiniteSparseMatrix(new_entries, 0.0)
 
     def __sub__(self, B : 'FiniteSparseMatrix') -> 'FiniteSparseMatrix':
         return A + (-1)*B
