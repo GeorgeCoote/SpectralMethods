2f1b15d1473bfa5fe52e81da07e40136430ccdfd
George Coote <108678340+GeorgeCoote@users.noreply.github.com>
2025-12-26 11:50:55 +0000

    Update and rename SparseMatrix.finite.py to FiniteSparseMatrix.py

---
+++ tools/FiniteSparseMatrix.py
@@ -0,0 +1,53 @@
+class FiniteSparseMatrix:
+    def __init__(self, non_zero_entries : dict[tuple[int, int], float], default_value : float = 0.0):
+        self.entries = non_zero_entries
+        self.default = default_value 
+        
+    def __call__(self, i : int, j : int) -> float:
+        return self.entries.get((i, j), self.default)
+    
+    def __add__(self, B : 'FiniteSparseMatrix') -> 'FiniteSparseMatrix':
+        new_default = self.default + B.default
+        self_size = len(self.entries)
+        B_size = len(B.entries)
+        smaller, bigger = self, B if self_size <= B_size else B, self
+        new_entries = bigger.entries.copy()
+        for idx in smaller.entries:
+            candidate = new_entries.get(idx, 0.0) + smaller.entries[idx]
+            if abs(candidate - new_default) < 1e-16:
+                new_entries.pop(idx, None)
+            else:
+                new_entries[idx] = candidate                
+        return FiniteSparseMatrix(new_entries, new_default)
+
+    def __mul__(self, c : float) -> 'FiniteSparseMatrix':
+        if c < 1e-16:
+            return FiniteSparseMatrix({}, 0.0)
+        else:
+            new_entries = {key : c*val for key, val in self.entries.items()}
+            new_default = c*self.default
+            return FiniteSparseMatrix(new_entries, new_default)
+
+    def __sub__(self, B : 'FiniteSparseMatrix') -> 'FiniteSparseMatrix':
+        return A + (-1)*B
+
+    def __repr__(self) -> str:
+        return f"FiniteSparseMatrix({len(self.entries)} entries, {self.default})"
+       
+    def get_entries(self) -> dict[tuple[int, int], float]:
+        return self.entries 
+    
+    def set_entries(self, new_entries : dict[tuple[int, int], float]) -> None:
+        for idx, val in new_entries.items():
+            self.entries[idx] = val
+
+    def pop(self, pos : dict[tuple[int, int], float]) -> None:
+        self.entries.pop(pos, None)
+    
+    def get_default(self):
+        return self.default
+    
+    def set_default(self, new_default : float) -> None:
+        self.default = new_default
+
+    
